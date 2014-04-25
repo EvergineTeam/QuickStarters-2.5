6bc20433b207b066de001e5184afc34469a4292f
WaveEngine Team <waveengineteam@gmail.com>
2014-04-25 11:29:43 +0200

    Update MainPage.xaml.cs

--- SuperSquid/LauncherWP/MainPage.xaml.cs
+++ SuperSquid/LauncherWP/MainPage.xaml.cs
@@ -2,7 +2,6 @@ using System.Security;
 using Microsoft.Phone.Controls;
 using System.Windows.Controls;
 using System.Windows.Media;
-using GoogleAds;
 using WaveEngine.Adapter;
 using System.Diagnostics;
 
@@ -24,31 +23,11 @@ namespace LauncherWP
 
             DrawingSurface.SetBackgroundContentProvider(application.ContentProvider);
             DrawingSurface.SetBackgroundManipulationHandler(application.ManipulationHandler);
-
-            // Admob
-            AdRequest adRequest = new AdRequest();
-
-            adRequest.ForceTesting = true;
-            this.admob.LoadAd(adRequest);
         }
 
         protected override void OnOrientationChanged(OrientationChangedEventArgs e)
         {
             base.OnOrientationChanged(e);
         }
-
-        #region Admob Methods
-
-        private void OnAdReceived(object sender, AdEventArgs e)
-        {
-            Debug.WriteLine("Received ad successfully");
-        }
-
-        private void OnFailedToReceiveAd(object sender, AdErrorEventArgs errorCode)
-        {
-            Debug.WriteLine("Failed to receive ad with error " + errorCode.ErrorCode);
-        }
-
-        #endregion
     }
 }
