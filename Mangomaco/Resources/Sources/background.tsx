9d67ad42ecf60b0964a0f373918799fac0d6c1b1
Jorgemagic <jorgemagic@gmail.com>
2015-08-19 12:53:58 +0200

    - Removed non-ported quickstarters

--- Mangomaco/Resources/Sources/background.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="background" tilewidth="16" tileheight="16">
- <image source="../Content/tilesets/background.png" width="164" height="89"/>
-</tileset>
--- Mangomaco/Resources/Sources/sceneTiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="sceneTiles" tilewidth="16" tileheight="16" spacing="2" margin="1">
- <image source="../Content/tilesets/sceneTiles.png" width="306" height="306"/>
-</tileset>
