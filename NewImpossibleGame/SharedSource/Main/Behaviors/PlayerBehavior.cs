22ed495d854a70ac7112efb7efa7f1d8ea58dc67
Javier Fernández Rodríguez <jfrodriguez@plainconcepts.com>
2015-11-17 17:27:05 +0100

    - Added ImpossibleGame QuickStarter

---
+++ NewImpossibleGame/SharedSource/Main/Behaviors/PlayerBehavior.cs
@@ -0,0 +1,318 @@
+﻿using NewImpossibleGame.Enums;
+using System;
+using System.Runtime.Serialization;
+using NewImpossibleGame.Services;
+using WaveEngine.Common.Attributes;
+using WaveEngine.Common.Input;
+using WaveEngine.Common.Math;
+using WaveEngine.Framework;
+using WaveEngine.Framework.Graphics;
+using WaveEngine.Framework.Services;
+
+namespace NewImpossibleGame.Behaviors
+{
+    /// <summary>
+    /// Player behavior
+    /// </summary>
+    [DataContract]
+    public class PlayerBehavior : Behavior
+    {
+        /// <summary>
+        /// The player transform
+        /// </summary>
+        [RequiredComponent]
+        private Transform3D playerTransform = null;
+
+        /// <summary>
+        /// The game behavior
+        /// </summary>
+        [RequiredComponent]
+        private GameBehavior gameBehavior = null;
+
+        /// <summary>
+        /// The player state
+        /// </summary>
+        private PlayerState playerState = PlayerState.INITIAL;
+
+        /// <summary>
+        /// The current vertical velocity
+        /// </summary>
+        private float currentVerticalVelocity;
+
+        /// <summary>
+        /// The boost velocity
+        /// </summary>
+        public float BoostVelocity;
+
+        /// <summary>
+        /// Gets or sets the game controller entity.
+        /// </summary>
+        /// <value>
+        /// The game controller entity.
+        /// </value>
+        [DataMember]
+        [RenderPropertyAsEntity]
+        public string GameControllerEntitySource
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the player velocity.
+        /// </summary>
+        /// <value>
+        /// The player velocity.
+        /// </value>
+        [DataMember]
+        public float PlayerVelocity
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the jump speed.
+        /// </summary>
+        /// <value>
+        /// The jump speed.
+        /// </value>
+        [DataMember]
+        public float JumpSpeed
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the gravity.
+        /// </summary>
+        /// <value>
+        /// The gravity.
+        /// </value>
+        [DataMember]
+        public float Gravity
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the boost deceleration.
+        /// </summary>
+        /// <value>
+        /// The boost deceleration.
+        /// </value>
+        [DataMember]
+        public float BoostDeceleration
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the boost acceleration.
+        /// </summary>
+        /// <value>
+        /// The boost acceleration.
+        /// </value>
+        [DataMember]
+        public float BoostAcceleration
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the player initial position.
+        /// </summary>
+        /// <value>
+        /// The player initial position.
+        /// </value>
+        [DataMember]
+        public Vector3 PlayerInitialPosition
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// The player bounding box
+        /// </summary>
+        public BoundingBox PlayerBoundingBox;
+
+        /// <summary>
+        /// Default Values, those will be override by WaveEditor (in case any of them were setted).
+        /// </summary>
+        protected override void DefaultValues()
+        {
+            base.DefaultValues();
+
+            this.JumpSpeed = 20.0f;
+            this.Gravity = -70.0f;
+            this.PlayerVelocity = 15.0f;
+            this.BoostDeceleration = 7.0f;
+            this.BoostAcceleration = 10.0f;
+            this.PlayerBoundingBox = new BoundingBox();
+        }
+
+        /// <summary>
+        /// Allows this instance to execute custom logic during its
+        /// <c>Update</c>.
+        /// </summary>
+        /// <param name="gameTime">The game time.</param>
+        /// <remarks>
+        /// This method will not be executed if the
+        /// <see cref="T:WaveEngine.Framework.Component" />, or the
+        /// <see cref="T:WaveEngine.Framework.Entity" />
+        /// owning it are not
+        /// <c>Active</c>.
+        /// </remarks>
+        protected override void Update(TimeSpan gameTime)
+        {
+            // convert here the gametime in float, multiple uses in this method
+            float elapsedSeconds = (float)gameTime.TotalSeconds;
+            Vector3 position = this.playerTransform.Position;
+
+            // Calculate Position
+            position.Z += this.CalculateCurrentVelocity(elapsedSeconds);
+
+            // update player bounding box
+            this.PlayerBoundingBox.Min.Y = this.playerTransform.Position.Y - ModelFactoryService.Instance.Scale.Y / 2;
+            this.PlayerBoundingBox.Max.Y = this.playerTransform.Position.Y + ModelFactoryService.Instance.Scale.Y / 2;
+            this.PlayerBoundingBox.Min.Z = this.playerTransform.Position.Z - ModelFactoryService.Instance.Scale.Z / 2;
+            this.PlayerBoundingBox.Max.Z = this.playerTransform.Position.Z + ModelFactoryService.Instance.Scale.Z / 2;
+
+            // 4 game states machine
+            switch (this.playerState)
+            {
+                // Initial State: Initial player configuration
+                case PlayerState.INITIAL:
+                    position = this.PlayerInitialPosition;
+                    this.BoostVelocity = 0.0f;
+                    this.currentVerticalVelocity = 0f;
+                    this.playerState = PlayerState.GROUND;
+                    break;
+
+                // Player is touching ground (plane where allow to walk)
+                case PlayerState.GROUND:
+                    // floor level is under player then fall
+                    if (this.gameBehavior.CurrentGroundLevel + ModelFactoryService.Instance.Scale.Y / 2 < this.playerTransform.Position.Y)
+                    {
+                        this.playerState = PlayerState.FALLING;
+                    }
+
+                    // Check the game keys
+                    if (WaveServices.Input.KeyboardState.Up == ButtonState.Pressed
+                        || WaveServices.Input.KeyboardState.Space == ButtonState.Pressed
+                        || WaveServices.Input.TouchPanelState.Count > 0)
+                    {
+                        this.currentVerticalVelocity = this.JumpSpeed;
+                        this.playerState = PlayerState.JUMPING;
+                        break;
+                    }
+                    break;
+
+                // playing is jumping, ascending!!
+                case PlayerState.JUMPING:
+                    // vertical acceleration and rotation (elapsed time relative, care with this)
+                    position.Y += this.currentVerticalVelocity * elapsedSeconds;
+
+                    // update vertical velocity with the gravity acceleration
+                    this.currentVerticalVelocity += this.Gravity * elapsedSeconds;
+
+                    // if finish jumping (negative vertical velocity) then we are falling!!
+                    if (this.currentVerticalVelocity < 0)
+                    {
+                        this.playerState = PlayerState.FALLING;
+                    }
+                    break;
+
+                // player falls down, after a jump of running over an empty block (then should fall anyway)
+                case PlayerState.FALLING:
+                    // its a free fall acceleration: v(t)=a*t
+                    this.currentVerticalVelocity += this.Gravity * elapsedSeconds;
+                    position.Y += this.currentVerticalVelocity * elapsedSeconds;
+
+                    // check if we touch the ground of current
+                    if (this.playerTransform.Position.Y <= this.gameBehavior.CurrentGroundLevel + ModelFactoryService.Instance.Scale.Y * 0.7)
+                    {
+                        position.Y = this.gameBehavior.CurrentGroundLevel + ModelFactoryService.Instance.Scale.Y / 2;
+                        this.playerState = PlayerState.GROUND;
+                    }
+
+                    // check if we touch the ground of next
+                    if (this.playerTransform.Position.Y <= this.gameBehavior.NextGroundLevel + ModelFactoryService.Instance.Scale.Y * 0.7)
+                    {
+                        position.Y = this.gameBehavior.NextGroundLevel + ModelFactoryService.Instance.Scale.Y / 2;
+                        this.playerState = PlayerState.GROUND;
+                    }
+                    break;
+
+                // Player DIE! no special treatment here
+                case PlayerState.DIE:
+                    break;
+            }
+
+            this.playerTransform.Position = position;
+        }
+
+        /// <summary>
+        /// Restarts this instance.
+        /// </summary>
+        public void Restart()
+        {
+            this.playerState = PlayerState.INITIAL;
+        }
+
+        /// <summary>
+        /// Calculates the player velocity.
+        /// </summary>
+        /// <param name="elapsedGameTime">The elapsed game time.</param>
+        private float CalculateCurrentVelocity(float elapsedGameTime)
+        {
+            // New Player Position
+            // delta movement using boostvelocity
+            var delta = (this.PlayerVelocity * elapsedGameTime) + this.BoostVelocity;
+
+            // check boost velocity to decelerate
+            if (this.BoostVelocity > 0.0)
+            {
+                this.BoostVelocity -= BoostDeceleration * elapsedGameTime;
+
+                // minimum boost is 0.0
+                if (this.BoostVelocity < 0.0f)
+                {
+                    this.BoostVelocity = 0.0f;
+                }
+            }
+
+            return delta;
+        }
+
+        /// <summary>
+        /// Accelerates the player.
+        /// </summary>
+        /// <param name="elapsedSeconds">The p.</param>
+        public void Accelerate(float elapsedSeconds)
+        {
+            this.BoostVelocity += this.BoostAcceleration * elapsedSeconds;
+
+            if (this.BoostVelocity > this.PlayerVelocity + this.BoostAcceleration)
+            {
+                this.BoostVelocity = this.PlayerVelocity + this.BoostAcceleration;
+            }
+        }
+
+        /// <summary>
+        /// Collideses the specified block transform.
+        /// </summary>
+        /// <param name="blockTransform">The block transform.</param>
+        /// <returns></returns>
+        public bool Collides(Transform3D blockTransform)
+        {
+            bool res =(this.playerTransform.Position.Y - ModelFactoryService.Instance.Scale.Y / 2 < blockTransform.Position.Y);
+            return res;
+        }
+    }
+}
---
+++ NewImpossibleGame/SharedSource/Main/Services/ModelFactoryService.cs
@@ -0,0 +1,252 @@
+﻿using NewImpossibleGame.Enums;
+using NewImpossibleGame.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using WaveEngine.Common.Graphics;
+using WaveEngine.Common.Math;
+using WaveEngine.Components.Graphics3D;
+using WaveEngine.Framework;
+using WaveEngine.Framework.Graphics;
+using WaveEngine.Framework.Managers;
+using WaveEngine.Framework.Physics3D;
+using WaveEngine.Materials;
+
+namespace NewImpossibleGame.Services
+{
+    /// <summary>
+    /// Model Factory with entity pool
+    /// </summary>
+    public class ModelFactoryService
+    {
+        // Singleton instance object
+        private static ModelFactoryService instance;
+
+        /// <summary>
+        /// The columntag
+        /// </summary>
+        public const string COLUMNTAG = "Column";
+
+        /// <summary>
+        /// the block pool
+        /// </summary>
+        private Dictionary<BlockTypeEnum, Stack<Entity>> blockPool;
+
+        /// <summary>
+        /// The column cache
+        /// </summary>
+        private Queue<Entity> columnCache;
+
+        /// <summary>
+        /// The scale
+        /// </summary>
+        public Vector3 Scale;
+
+        /// <summary>
+        /// Gets the instance of the Singleton.
+        /// </summary>
+        /// <value>
+        /// The instance.
+        /// </value>
+        public static ModelFactoryService Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new ModelFactoryService();
+                }
+                return instance;
+            }
+        }
+
+        /// <summary>
+        /// Prevents a default instance of the <see cref="ModelFactoryService" /> class from being created.
+        /// </summary>
+        private ModelFactoryService()
+        {
+            // Initializes BlockPool
+            this.blockPool = new Dictionary<BlockTypeEnum, Stack<Entity>>();
+            foreach (var type in Enum.GetValues(typeof(BlockTypeEnum)))
+            {
+                this.blockPool.Add((BlockTypeEnum)type, new Stack<Entity>());
+            }
+
+            this.columnCache = new Queue<Entity>();
+        }
+
+        /// <summary>
+        /// Creates a new column with recycled components.
+        /// </summary>
+        /// <param name="column">The column.</param>
+        /// <param name="zPosition">The z position.</param>
+        /// <returns></returns>
+        public Entity CreateColumn(Column column, float zPosition, EntityManager eManager)
+        {
+            Entity entityColumn = null;
+
+            // Column Cache
+            if (this.columnCache.Count > 0)
+            {
+                entityColumn = this.columnCache.Dequeue();
+            }
+            else
+            {
+                entityColumn = new Entity()
+                    {
+                        Tag = COLUMNTAG
+                    }
+                    .AddComponent(new Transform3D());
+
+                eManager.Add(entityColumn);
+            }
+
+            var transform = entityColumn.FindComponent<Transform3D>();
+            Vector3 positiontemp = transform.Position;
+            positiontemp.Z = zPosition;
+            transform.Position = positiontemp;
+            entityColumn.IsVisible = true;
+
+            // column element iteration to block entity Creation
+            for (int i = 0; i < column.Count; i++)
+            {
+                var position = new Vector3(0, i * this.Scale.Z, 0);
+                Color color = Color.Black;
+
+                switch (column[i])
+                {
+                    case BlockTypeEnum.GROUND:
+                        color = Color.WhiteSmoke;
+                        break;
+                    case BlockTypeEnum.BOX:
+                        color = Color.BlueViolet;
+                        break;
+                    case BlockTypeEnum.PYRAMID:
+                        color = Color.Red;
+                        break;
+                    case BlockTypeEnum.SPEEDERBLOCK:
+                        color = Color.Yellow;
+                        break;
+                    case BlockTypeEnum.EMPTY:
+                    default:
+                        break;
+                }
+
+                // add child to column
+                if (column[i] != BlockTypeEnum.EMPTY)
+                {
+                    entityColumn.AddChild(this.CreateBlock(position, color, this.Scale, column[i]));
+                }
+            }
+
+            return entityColumn;
+        }
+
+        /// <summary>
+        /// Frees the column from parent. Don't remove the element cause it calls Dispose, and we need recycle the object at the pool
+        /// </summary>
+        /// <param name="entityColumn">The entity column.</param>
+        public void FreeColumn(Entity entityColumn)
+        {
+            // Creates a list (to allow iterate)
+            var tempChildrenCollection = entityColumn.ChildEntities.ToList();
+
+            foreach (Entity sub in tempChildrenCollection)
+            {
+                // Detach the entity from the parent
+                entityColumn.DetachChild(sub.Name);
+
+                // add the free block to the pool
+                BlockTypeEnum blockType = BlockTypeEnum.EMPTY;
+                if (Enum.TryParse<BlockTypeEnum>(sub.Tag, out blockType))
+                {
+                    this.blockPool[blockType].Push(sub);
+                }
+
+                sub.IsVisible = false;
+                sub.IsActive = false;
+            }
+
+            this.columnCache.Enqueue(entityColumn);
+            entityColumn.IsVisible = false;
+        }
+
+        /// <summary>
+        /// Creates the Block.
+        /// </summary>
+        /// <param name="position">The position.</param>
+        /// <param name="color">The color.</param>
+        /// <param name="scale">The scale.</param>
+        /// <param name="entityType">Type of the entity.</param>
+        /// <returns></returns>
+        public Entity CreateBlock(Vector3 position, Color color, Vector3 scale, BlockTypeEnum entityType)
+        {
+            Entity entity = null;
+
+            // Check if there are any block free in the pool of the type we need
+            if (this.blockPool[entityType].Count > 0)
+            {
+                // pop from pool
+                entity = this.blockPool[entityType].Pop();
+
+                // Reset visibility and active property
+                entity.IsVisible = true;
+                var modelRenderer = entity.FindComponent<ModelRenderer>();
+                modelRenderer.IsVisible = true;
+                entity.IsActive = true;
+
+                // reset position
+                Transform3D transform = entity.FindComponent<Transform3D>();
+                transform.LocalPosition = position;
+                transform.Scale = scale;
+
+                // IMPORTANT: DO NOT REMOVE!! We need to Refresh boundbox of the model to locate it at right position.
+                entity.FindComponent<Model>(false).BoundingBoxRefreshed = true;
+
+                // change color map if needed
+                MaterialsMap materialMap = entity.FindComponent<MaterialsMap>();
+                StandardMaterial basicMaterial = (StandardMaterial)materialMap.DefaultMaterial;
+                basicMaterial.DiffuseColor = color;
+            }
+            else
+            {
+                // If there are not entities in the pool of the type we need, then we should create one
+                Model entityModel = null;
+
+                // It will be better to use FBX models than primitives to optimize (primitives do not share vertexmodel)
+                // but we use primitives here
+                switch (entityType)
+                {
+                    case BlockTypeEnum.GROUND:
+                    case BlockTypeEnum.BOX:
+                    case BlockTypeEnum.SPEEDERBLOCK:
+                        entityModel = Model.CreateCube();
+                        break;
+                    case BlockTypeEnum.PYRAMID:
+                        entityModel = Model.CreateCone();
+                        break;
+                    default:
+                    case BlockTypeEnum.EMPTY:
+                        // SHOULD NOT HAPPEND, this method NEVER will be called if BlockType is EMPTY... but in case we forgotten this then it's controlled
+                        break;
+                }
+
+                // Creates the new entity
+                if (entityModel != null)
+                {
+                    entity = new Entity()
+                        .AddComponent(new Transform3D() { Position = position, Scale = scale })
+                        .AddComponent(new MaterialsMap(new StandardMaterial(color, DefaultLayers.Opaque) { LightingEnabled = true, AmbientColor = Color.White * 0.25f }))
+                        .AddComponent(entityModel)
+                        .AddComponent(new BoxCollider3D())
+                        .AddComponent(new ModelRenderer());
+                    entity.Tag = entityType.ToString();
+
+                    // DO NOT push in pool here!!, only add to cache on remove!
+                    // this.blockPool[entityType].Push(entity);
+                }
+            }
+            return entity;
+        }
+    }
+}
