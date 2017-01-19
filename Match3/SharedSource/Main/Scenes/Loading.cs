b0690e864e96a6e9d21ff8e10052808c54e9a48b
Victor Ferrer <victorfergar@gmail.com>
2017-01-19 17:15:07 +0100

    Placeholder loading screen included

--- Match3/SharedSource/Main/Scenes/Loading.cs
+++ Match3/SharedSource/Main/Scenes/Loading.cs
@@ -1,4 +1,8 @@
+using Match3.Services;
+using Match3.Services.Navigation;
+using System;
 using WaveEngine.Framework;
+using WaveEngine.Framework.Services;
 
 namespace Match3.Scenes
 {
@@ -6,6 +10,17 @@ namespace Match3.Scenes
     {
         protected override void CreateScene()
         {
+            this.Load(WaveContent.Scenes.Loading);
+        }
+
+        protected override void Start()
+        {
+            base.Start();
+
+            WaveServices.TimerFactory.CreateTimer(TimeSpan.FromSeconds(1), () =>
+            {
+                CustomServices.NavigationService.Navigate(NavigateCommands.DefaultForward);
+            }, false, this);
         }
     }
 }
