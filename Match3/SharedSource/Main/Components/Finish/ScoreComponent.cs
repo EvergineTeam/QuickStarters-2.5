b27c6fc2a4ebcc27f00d421593aaf1bd72431c77
Daniel Cáceres <daniel.caceresm@outlook.com>
2017-03-14 17:40:32 +0100

    Level complete scene bindings to data.

---
+++ Match3/SharedSource/Main/Components/Finish/ScoreComponent.cs
@@ -0,0 +1,21 @@
+﻿using Match3.Services;
+using System.Runtime.Serialization;
+using WaveEngine.Components.Toolkit;
+using WaveEngine.Framework;
+
+namespace Match3.Components.Finish
+{
+    [DataContract]
+    public class ScoreComponent : Component
+    {
+        [RequiredComponent]
+        protected TextComponent textComponent;
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            this.textComponent.Text = CustomServices.GameLogic.CurrentScore.ToString();
+        }
+    }
+}
---
+++ Match3/SharedSource/Main/Components/Finish/StarsComponent.cs
@@ -0,0 +1,24 @@
+﻿using Match3.Services;
+using System.Linq;
+using System.Runtime.Serialization;
+using WaveEngine.Framework;
+
+namespace Match3.Components.Finish
+{
+    [DataContract]
+    public class StarsComponent : Component
+    {
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            var gameLogic = CustomServices.GameLogic;
+            var childStars = this.Owner.FindChildrenByTag("star").OrderBy(x => x.Name).Select(x => x.FindChild("inner")).ToArray();
+            for (int i = 0; i < childStars.Length; i++)
+            {
+                childStars[i].IsVisible = i < gameLogic.StarsScores.Length 
+                                       && gameLogic.StarsScores[i] <= gameLogic.CurrentScore;
+            }
+        }
+    }
+}
