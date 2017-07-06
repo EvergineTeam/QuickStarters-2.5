e1d0b08e8d38c2276612a52d851fdf810593dd9a
Javier Fernández Rodríguez <jfrodriguez@plainconcepts.com>
2017-07-06 14:25:19 +0200

    - Added P2p support - created button

--- P2PTank/P2PTank/SharedSource/Main/Scenes/P2PScene.cs
+++ P2PTank/P2PTank/SharedSource/Main/Scenes/P2PScene.cs
@@ -1,5 +1,6 @@
 #region Using Statements
 using System;
+using P2PNET.TransportLayer.EventArgs;
 using WaveEngine.Common;
 using WaveEngine.Common.Graphics;
 using WaveEngine.Common.Math;
@@ -10,15 +11,47 @@ using WaveEngine.Framework;
 using WaveEngine.Framework.Graphics;
 using WaveEngine.Framework.Resources;
 using WaveEngine.Framework.Services;
+using WaveEngine.Framework.UI;
+using WaveEngine.Networking.P2P;
 #endregion
 
 namespace P2PTank.Scenes
 {
     public class P2PScene : Scene
     {
+        private Peer2Peer peerManager;
+
         protected override void CreateScene()
         {
             this.Load(WaveContent.Scenes.MyScene);
+
+            this.peerManager = new Peer2Peer();
+            this.peerManager.PeerChange +=  this.OnPeerChanged;
+            this.peerManager.MsgReceived += this.OnMsgReceived;
+
+            var button = new WaveEngine.Components.UI.Button()
+            {
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center,
+                Text = "START",
+                IsBorder = false,
+            };
+
+            button.Click += this.OnStartButtonClicked;
+            this.EntityManager.Add(button);
+        }
+
+        private async void OnStartButtonClicked(object sender, EventArgs e)
+        {
+            await this.peerManager.StartBroadcastingAsync();
+        }
+
+        private void OnMsgReceived(object sender, MsgReceivedEventArgs e)
+        {
+        }
+
+        private void OnPeerChanged(object sender, PeerChangeEventArgs e)
+        {
         }
     }
 }
