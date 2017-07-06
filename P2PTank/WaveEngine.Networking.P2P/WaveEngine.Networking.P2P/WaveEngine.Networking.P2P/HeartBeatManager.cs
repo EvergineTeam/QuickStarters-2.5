fd574d49b8cb9e744a83d126456f83ce459164a2
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2017-07-06 14:09:18 +0200

    Initial WaveEngine.Networking.P2P library and created P2PTank project game.

---
+++ P2PTank/WaveEngine.Networking.P2P/WaveEngine.Networking.P2P/WaveEngine.Networking.P2P/HeartBeatManager.cs
@@ -0,0 +1,36 @@
+﻿using P2PNET.TransportLayer;
+using System.Diagnostics;
+using System.Text;
+
+namespace WaveEngine.Networking.P2P
+{
+    public class HeartBeatManager
+    {
+        private string heartBeatMsg;
+        private TransportManager transMgr;
+        private Timer hrtBtTimer;
+
+        public HeartBeatManager(string mHeartBeatMsg, TransportManager mTransMgr)
+        {
+            this.heartBeatMsg = mHeartBeatMsg;
+            this.transMgr = mTransMgr;
+        }
+
+        public void StartBroadcasting()
+        {
+            this.hrtBtTimer = new Timer(TimerCallBack, null, 0, 1000);
+        }
+
+        public void EndBroadcasting()
+        {
+            this.hrtBtTimer.Cancel();
+        }
+        
+        private async void TimerCallBack(object parameter)
+        {
+            byte[] msgBin = Encoding.UTF8.GetBytes(heartBeatMsg);
+            await this.transMgr.SendBroadcastAsyncUDP(msgBin);
+            Debug.WriteLine("Sent heartbeat");
+        }
+    }
+}
