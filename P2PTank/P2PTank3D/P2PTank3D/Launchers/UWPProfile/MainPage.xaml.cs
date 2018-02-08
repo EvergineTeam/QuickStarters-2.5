52187667a6264c874ade2a441e0f9dd9351351f4
PLAINCONCEPTS\jfrodriguez <jfrodriguez@plainconcepts.com>
2018-02-08 16:38:21 +0100

    - Fixed Color issue on network game

--- P2PTank/P2PTank3D/P2PTank3D/Launchers/UWPProfile/MainPage.xaml.cs
+++ P2PTank/P2PTank3D/P2PTank3D/Launchers/UWPProfile/MainPage.xaml.cs
@@ -1,5 +1,7 @@
+using System.Diagnostics;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,6 +24,20 @@ namespace P2PTank3D
         {
             this.application = new GameRenderer(this.SwapChainPanel);
             this.application.Initialize();
+        }
+
+        protected override void OnKeyDown(KeyRoutedEventArgs e)
+        {
+            base.OnKeyDown(e);
+
+            Debug.WriteLine("OnKeyDown");
+        }
+
+        protected override void OnKeyUp(KeyRoutedEventArgs e)
+        {
+            base.OnKeyUp(e);
+
+            Debug.WriteLine("OnKeyUp");
         }
     }
 }
