353db80d82b3b4bb213028958335e2d010ae9b46
Javier Suárez Ruiz <javiersuarezruiz@hotmail.com>
2018-02-27 16:35:48 +0100

    Fixed Victory Network Sync

--- P2PTank/P2PTank3D/P2PTank3D/SharedSource/Main/Models/LeaderBoard.cs
+++ P2PTank/P2PTank3D/P2PTank3D/SharedSource/Main/Models/LeaderBoard.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Runtime.Serialization;
-using System.Text;
 using WaveEngine.Common.Graphics;
-using WaveEngine.Components.UI;
 using WaveEngine.Framework;
 using WaveEngine.Framework.Graphics;
 
